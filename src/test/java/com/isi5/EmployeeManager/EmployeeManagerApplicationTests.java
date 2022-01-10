f7007b8a49f530a8120f257f38490b3f7085c880
yasTheDreamer <yassine19265@gmail.com>
2022-01-10 20:01:59 +0100

    initial commit

---
+++ src/test/java/com/isi5/EmployeeManager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi5.EmployeeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
